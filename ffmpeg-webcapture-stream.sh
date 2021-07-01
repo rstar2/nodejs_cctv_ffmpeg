c0db23817d8e06e2fa562407254ad45011c650a7
Rumen Neshev <rstar2@abv.bg>
2021-07-01 16:52:14 +0300

    fix env var names

--- ffmpeg-webcapture-stream.sh
+++ ffmpeg-webcapture-stream.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-# get PORT_STREAM env variable and if not fallback to 9999
-PORT="${PORT_STREAM:-9999}"
+# get WEBCAM_PORT_STREAM env variable and if not fallback to 9999
+PORT="${WEBCAM_PORT_STREAM:-9999}"
 
 # video and audio
 # ffmpeg -f v4l2 -video_size 640x480 -framerate 30 -i /dev/video0 -f alsa -ar 44100 -ac 2 -i hw:0 -f mpegts -codec:v mpeg1video -s 640x480 -b:v 1000k -bf 0 -codec:a mp2 -b:a 128k -muxdelay 0.001 http://127.0.0.1:$PORT
--- server-ffmpeg.sh
+++ server-ffmpeg.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-PORT="${PORT_STREAM:-9999}"
+PORT="${WEBCAM_PORT_STREAM:-9999}"
 
 # wait max 20 secs (-t 20) for the server 127.0.0.1:8888 to be available
 # and ONLY then (-s) execute the subcommand (-- COMMAND ARGS)
